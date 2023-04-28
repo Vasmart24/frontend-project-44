74c2030576e59247109dfc21d8fcfc6740130b8e
vasmart24 <vasmart24@yandex.ru>
2023-04-28 16:41:48 +0300

    fix

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSynk from 'readline-sync';
 
 export default () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSynk.question('May i have your name? ');
+  const userName = readlineSynk.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
 };
